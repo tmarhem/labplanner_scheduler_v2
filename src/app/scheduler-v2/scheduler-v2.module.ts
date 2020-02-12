7b3e67e9dae7bffe490cd2edb19768010d824bce
tmarhem <eskel594@gmail.com>
2020-02-12 13:17:01 +0100

    DONE: missing files

--- src/app/scheduler-v2/scheduler-v2.module.ts
+++ src/app/scheduler-v2/scheduler-v2.module.ts
@@ -4,23 +4,23 @@ import { CommonModule } from '@angular/common';
 import { SchedulerComponent } from './scheduler/scheduler.component';
 import { SettingsComponent } from './settings/settings.component';
 import { TimeslotPickerComponent } from './timeslot-picker/timeslot-picker.component';
+import { SchedulerV2Component } from './scheduler-v2.component';
 
 import { MaterialModule } from './material.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    MaterialModule
+    // MaterialModule
   ],
   declarations: [
     SchedulerComponent,
     SettingsComponent,
-    TimeslotPickerComponent
+    TimeslotPickerComponent,
+    SchedulerV2Component
   ],
   exports: [
-    SchedulerComponent,
-    SettingsComponent,
-    TimeslotPickerComponent
+    SchedulerV2Component
   ]
 })
 export class SchedulerV2Module { }
