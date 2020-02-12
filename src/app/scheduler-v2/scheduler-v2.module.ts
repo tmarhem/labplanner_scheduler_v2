eee8df86cdfc40c06a9a3b78fef7fc5378ae9b06
tmarhem <eskel594@gmail.com>
2020-02-12 12:54:05 +0100

    DONE: Scheduler module

--- src/app/scheduler-v2/scheduler-v2.module.ts
+++ src/app/scheduler-v2/scheduler-v2.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SchedulerComponent } from './scheduler/scheduler.component';
+import { SettingsComponent } from './settings/settings.component';
+import { TimeslotPickerComponent } from './timeslot-picker/timeslot-picker.component';
+
+import { MaterialModule } from './material.module';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ],
-  declarations: []
+  declarations: [
+    SchedulerComponent,
+    SettingsComponent,
+    TimeslotPickerComponent
+  ],
+  exports: [
+    SchedulerComponent,
+    SettingsComponent,
+    TimeslotPickerComponent
+  ]
 })
 export class SchedulerV2Module { }
