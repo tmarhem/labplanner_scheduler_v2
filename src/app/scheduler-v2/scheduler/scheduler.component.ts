8983f207fe10acce68e45b317344a4aeab4605e2
tmarhem <eskel594@gmail.com>
2020-02-18 14:30:35 +0100

    WIP: refacto ng class

--- src/app/scheduler-v2/scheduler/scheduler.component.ts
+++ src/app/scheduler-v2/scheduler/scheduler.component.ts
@@ -1,13 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
-
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-scheduler',
-  templateUrl: './scheduler.component.html',
-  styleUrls: ['./scheduler.component.css']
+  selector: "app-scheduler",
+  templateUrl: "./scheduler.component.html",
+  styleUrls: ["./scheduler.component.css"]
 })
 export class SchedulerComponent implements OnInit {
-
   @Input() headers: Array<Array<any>>;
   @Input() rows: Array<any>;
 
@@ -19,33 +17,34 @@ export class SchedulerComponent implements OnInit {
   spannedRows: Array<any>;
   codesList: Array<string>;
 
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    this.firstHeadersList = this.headers[0].map( h => h.code);
-    this.secondHeadersList = this.headers[1].map( h => h.code);
-    this.thirdHeadersList = this.headers[2].map( h => h.code);
-    this.headersCodes = this.headers.map( headerRow => 
-      headerRow.map( header => header.code)
+    this.firstHeadersList = this.headers[0].map(h => h.code);
+    this.secondHeadersList = this.headers[1].map(h => h.code);
+    this.thirdHeadersList = this.headers[2].map(h => h.code);
+    this.headersCodes = this.headers.map(headerRow =>
+      headerRow.map(header => header.code)
     );
   }
 
   handleError = (e: any) => {
-    console.log( 'handleError', e);
-  }
+    console.log("handleError", e);
+  };
 
   getTimeSlotDisplayValue = (row, code) => {
-    if( !row[code]){ return null};
+    if (!row[code]) {
+      return null;
+    }
     if (row[code].value) return row[code].value;
-    if (typeof row[code] === 'string') return row[code];
-    return 'NA';
-  }
+    if (typeof row[code] === "string") return row[code];
+    return "NA";
+  };
 
   //TODO to not span same text on different types
   isSameTimeslot = (t1, t2): boolean => {
     return false;
-  }
+  };
 
   /**
    * Returns the spanning for a defined cell, if 0 the cell should be hidden
@@ -56,31 +55,60 @@ export class SchedulerComponent implements OnInit {
    * isLast: boolean, true if last col
    */
   getRowSpan = (row, colIndex, code, isFirst, isLast) => {
-    try{
+    try {
+      const isSelectionCell = row[code] === "";
 
-    const isSelectionCell = row[code] === "";
-    if( isFirst || isSelectionCell ) {return 1;};
+      if (isFirst || isSelectionCell) {
+        return 1;
+      }
 
-    let span = 0;
-    let currentCellValue = this.getTimeSlotDisplayValue(row, this.firstHeadersList[colIndex]);
-    let previousCellValue = this.getTimeSlotDisplayValue(row, this.firstHeadersList[colIndex -1]);
-    let isPreviousCellSameValue = currentCellValue === previousCellValue ;
-    if( isPreviousCellSameValue ) {return 0;};
+      let span = 0;
+      let currentCellValue = this.getTimeSlotDisplayValue(
+        row,
+        this.headersCodes[0][colIndex]
+      );
+      let previousCellValue = this.getTimeSlotDisplayValue(
+        row,
+        this.headersCodes[0][colIndex - 1]
+      );
+      let isPreviousCellSameValue = currentCellValue === previousCellValue;
 
-    let nextCellValue;
-    let isNextCellSameValue;
+      if (isPreviousCellSameValue) {
+        return 0;
+      }
 
-    do {
-      currentCellValue = this.getTimeSlotDisplayValue(row, this.firstHeadersList[colIndex + span]);
-      nextCellValue = this.getTimeSlotDisplayValue(row, this.firstHeadersList[colIndex + 1 + span]);
-      isNextCellSameValue = currentCellValue === nextCellValue;
-      span ++;
-    } while ( isNextCellSameValue );
+      let nextCellValue;
+      let isNextCellSameValue;
 
-    return span;
+      do {
+        currentCellValue = this.getTimeSlotDisplayValue(
+          row,
+          this.headersCodes[0][colIndex + span]
+        );
+        nextCellValue = this.getTimeSlotDisplayValue(
+          row,
+          this.headersCodes[0][colIndex + 1 + span]
+        );
+        isNextCellSameValue = currentCellValue === nextCellValue;
+        span++;
+      } while (isNextCellSameValue);
+
+      return span;
     } catch (e) {
       this.handleError(e);
     }
-  }
+  };
 
+    getRowClasses = (row, colIndex, code, isFirst, isLast) => {
+      const classes = [];
+      const rowSpan = this.getRowSpan(row, colIndex, code, isFirst, isLast);
+      const category = row[code].category ? row[code].category : null;
+
+      if( category)
+      classes.push(category);
+      if ( rowSpan === 0) {
+        classes.push('hidden');
+      }
+      return classes;
+    }
 }
