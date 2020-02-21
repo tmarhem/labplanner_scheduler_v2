65ac5e464e36e837d5bbfeabff83af51cf757673
tmarhem <eskel594@gmail.com>
2020-02-21 10:42:03 +0100

    DONE: Create notification service

---
+++ src/app/scheduler-v2/_services/schedulerNorification.service.ts
@@ -0,0 +1,35 @@
+import { Subject } from 'rxjs';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class UnityNotificationService {
+    constructor() {}
+
+    private genericSource = new Subject<NotificationAction<any>>();
+    public slaAction = this.genericSource.asObservable(); // leaving access only to the observable so you cannot use/spoof the subject at will
+
+    public sendGenericAction = (slaNotification: NotificationAction<any>) => {
+        this.genericSource.next(slaNotification);
+    };
+}
+
+export class NotificationAction<T> {
+    constructor(type: Action, id?: number, data?: T) {
+        this.type = type;
+        this.id = id ? id : null;
+        this.data = data ? data : null;
+    }
+    data?: T;
+    id?: number;
+    type: Action;
+}
+
+export enum Action {
+    CREATE = 'CREATE',
+    DELETE = 'DELETE',
+    EDIT = 'EDIT',
+    SELECT = 'SELECT',
+    REFRESH = 'REFRESH',
+}
